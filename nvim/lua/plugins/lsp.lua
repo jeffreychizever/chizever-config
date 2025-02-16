4412c42551eef4962e5b41b44f2a70e0a1411541
Jeffrey Chizever <jeffreychizever@gmail.com>
2025-02-16 11:41:42 -0500

    get linters running

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,6 +5,8 @@ return {
         "mfussenegger/nvim-lint", "rshkarin/mason-nvim-lint",
         "nvim-java/nvim-java"},
     config = function()
+
+        -- ~~~ LSPs ~~~
         local mason = require("mason")
         mason.setup({
             PATH = "prepend"
@@ -21,7 +23,29 @@ return {
         local lspc = require("lspconfig")
         lspc.jdtls.setup({})
         lspc.lua_ls.setup({})
+        do end
 
+
+        -- ~~~ Linters ~~~
+        require('lint').linters_by_ft = {
+            python = {'flake8'},
+            ruby = {'rubocop'},
+            java = {'checkstyle'},
+            lua = {'luacheck'},
+            typescript = {'eslint_d'},
+            javascript = {'eslint_d'},
+            typescriptreact = {'eslint_d'},
+            javascriptreact = {'eslint_d'},
+        }
+
+        vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+        callback = function()
+                local lint_status, lint = pcall(require, "lint")
+                if lint_status then
+                    lint.try_lint()
+                end
+            end,
+        })
 
         require ('mason-nvim-lint').setup({
             ensure_installed = { "flake8", "rubocop", "eslint_d", "checkstyle", "luacheck" },
