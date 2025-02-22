b8184624047a175713f677a4884d74fa677a03c5
Jeffrey Chizever <jeffreychizever@gmail.com>
2025-02-22 11:47:13 -0500

    telescope from workspace root

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,13 @@ return {
         })
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+        vim.keymap.set('n', '<leader>ff', function()
+            builtin.find_files({
+                cwd = vim.fs.root(0, {
+                    '.git',
+                }) or vim.env.PWD
+            })
+        end, { desc = 'Find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
         vim.keymap.set('n', '<leader>f*', builtin.grep_string, { desc = 'Live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
